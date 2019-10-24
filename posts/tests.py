5d6c2069f24365192aa88e2fa6c5554fa460e807
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-24 11:19:05 +0300

    Write test for saving a profile

--- posts/tests.py
+++ posts/tests.py
@@ -10,3 +10,12 @@ class ProfileTestClass(TestCase):
         '''
         self.new_user = User.objects.create_user(username='user',password='user-password')
         self.new_profile = UserProfile(id=1,user=self.new_user,profile_pic='photos/pic',bio='user-bio')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,UserProfile))
+
+
+    def test_save_profile(self):
+        self.new_profile.save_profile()
+        profiles = UserProfile.objects.all()
+        self.assertTrue(len(profiles) > 0)
