21a213b67362f951fa879d84309a51f68ee88bd2
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-24 11:01:05 +0300

    Create Project model

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from tinymce.models import HTMLField
 from datetime import datetime
-from django.db import models
 from url_or_relative_url_field.fields import URLOrRelativeURLField
 from django.contrib.auth.models import User
 
@@ -25,4 +24,14 @@ class UserProfile(models.Model):
         self.save()        
 
     def __str__(self):
-        return self.user.username        
+        return self.user.username  
+
+class Project(models.Model):
+    project_title = models.CharField(max_length=40)
+    project_description = HTMLField()
+    landing_page = models.ImageField(upload_to='landing_pages')
+    live_site = URLOrRelativeURLField()
+    user = models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
+    design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)      
