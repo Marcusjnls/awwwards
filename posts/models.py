028bbcba458fd93a16b19ac723fee883791430a6
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-24 10:51:19 +0300

    Create Profile model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_photos',null=True,blank=True)
+    bio = HTMLField()
+    contact=models.CharField(max_length=12)
+    linkedIn =  URLOrRelativeURLField()
+    projects = models.ForeignKey('Project',on_delete=models.CASCADE,null=True)
