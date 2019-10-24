d77206a24f0c6844844f962063d40a8ee0518688
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-24 11:18:29 +0300

    Write set up method for test cases

--- posts/tests.py
+++ posts/tests.py
@@ -4,3 +4,9 @@ from .models import Project,UserProfile
 
 # Create your tests here.
 class ProfileTestClass(TestCase):
+    def setUp(self):
+        '''
+        Set Up method to run before each test case
+        '''
+        self.new_user = User.objects.create_user(username='user',password='user-password')
+        self.new_profile = UserProfile(id=1,user=self.new_user,profile_pic='photos/pic',bio='user-bio')
