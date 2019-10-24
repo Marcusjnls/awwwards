f8b66ca6ffc7ec9a14dc1dbc444cd3515f712696
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-24 11:08:31 +0300

    Import necessary modules and create a profile test class

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Project,UserProfile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
