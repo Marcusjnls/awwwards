fbd0265c723480f4128f46a116f61dddbb39b5c2
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-25 15:43:06 +0300

    Remove whitenoise module to fix error in deployment

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,6 +136,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
